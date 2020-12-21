7fd6ebb7d9d27d951a0e531c5b348b739cb68078
Sabin Caragea <sabincaragea2005@gmail.com>
2020-12-21 13:43:25 -0500

    redone, still has issues

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
-import linecache
+def readData(filename):
+    data = []
+    try:
+        # Opening the file
+        with open(filename) as file:
+            # Loop through line by line
+            for line in file:
+                # split line using separator, store it in variable s
+                s = line.split()
+                # if s is size 2
+                if s.size == 2:
+                    # set first element stripped to symptom
+                    symptom = s[0]
+                    # set second element  stripped to diagnosis
+                    diagnosis = s[1]
+                    # add to data a list of symptom, diagnosis
+                    data.append(symptom)
+                    data.append(diagnosis)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    return data
 
-def welcome():
-    return str(input("\nHello! My name is The Doctor. What seems to be the problem?"))
 
-def diagnosis():
-    symptom = welcome()
-    file = open('data.txt')
-    result = file.readlines
-    if symptom == "runny nose":
-        print(linecache.getline('data.txt',2))
-    elif symptom == "fever":
-        print(result[1])
-    elif symptom == "fever":
-        print(result[2])
-    elif symptom == "fever":
-        print(result[3])
-    else:
-        print("I'm sorry, we couldn't match your condition.")
-    file.close()
+def findDiagnosis(data, userInput):
+    found = False
+    # Loop through each element of data
+    for item in data:
+        symptom = item[0]
+        diagnosis = item[1]
+        # if symptom is in userInput
+        if symptom == userInput:
+            # print "You have {diagnosis}"
+            print("You have {diagnosis}")
+            # set found = true
+            found = True
 
-def exit():
-    print("Thank you for using The Doctor")
+    return found
+
 
 def main():
-    welcome()
-    diagnosis()
-    exit()
+    userInput = input(
+        "Hello! My name is The Doctor. What seems to be the problem?\n")
+    data = readData('data.txt')
+
+    found = findDiagnosis(data, userInput)
+    if not found:
+        print("I'm sorry, we couldn't match your condition.")
+
 
 if __name__ == "__main__":
     main()
