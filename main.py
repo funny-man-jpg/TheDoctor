521094fc358274c05049a9b2a2b3efe77e68ab95
Sabin Caragea <sabincaragea2005@gmail.com>
2020-12-22 12:17:29 -0500

    Semi-final version, program does not append to list

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ def readData(filename):
             # Loop through line by line
             for line in file:
                 # split line using separator, store it in variable s
-                s = line.split()
+                s = line.split("-->")
                 # if s is size 2
-                if s.size == 2:
+                if len(s) == 2:
                     # set first element stripped to symptom
-                    symptom = s[0]
+                    symptom = s[0].strip()
                     # set second element  stripped to diagnosis
-                    diagnosis = s[1]
+                    diagnosis = s[1].strip()
                     # add to data a list of symptom, diagnosis
-                    data.append(symptom)
-                    data.append(diagnosis)
+                    data.append([symptom, diagnosis])
     except Exception as e:
         print(f"Unexpected error: {e}")
     return data
@@ -28,9 +27,9 @@ def findDiagnosis(data, userInput):
         symptom = item[0]
         diagnosis = item[1]
         # if symptom is in userInput
-        if symptom == userInput:
+        if symptom in userInput:
             # print "You have {diagnosis}"
-            print("You have {diagnosis}")
+            print(f"You have {diagnosis}")
             # set found = true
             found = True
 
@@ -38,9 +37,10 @@ def findDiagnosis(data, userInput):
 
 
 def main():
+    data = readData('data.txt')
     userInput = input(
         "Hello! My name is The Doctor. What seems to be the problem?\n")
-    data = readData('data.txt')
+    
 
     found = findDiagnosis(data, userInput)
     if not found:
